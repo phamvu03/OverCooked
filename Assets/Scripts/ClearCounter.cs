cc56cf10b2c0af2ae9acaf59309ccd8997613c22
Vo Vo <quachviet03@gmail.com>
2024-05-20 19:13:52 +0700

    Fix Selected Counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoin;
+
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoin);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
