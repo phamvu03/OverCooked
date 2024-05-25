c4baca56afb2097bff60fe3de223205499393110
Vo Vo <quachviet03@gmail.com>
2024-05-25 17:29:57 +0700

    Game Main Menu Scence and Loading Scence

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader 
+{
+   
+
+    public enum Scence
+    {
+        MainMenuScence, 
+        GameScence, 
+        LoadingScence,
+    }
+
+    private static Scence targetScence;
+
+    public static void Load(Scence targetScence)
+    {
+        Loader.targetScence = targetScence;
+
+        SceneManager.LoadScene(Scence.LoadingScence.ToString());
+
+        
+    }
+
+    public static void LoaderCallBack()
+    {
+        SceneManager.LoadScene(targetScence.ToString());
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scence.GameScence);
+        });
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+
+}
